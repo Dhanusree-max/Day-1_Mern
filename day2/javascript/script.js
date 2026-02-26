a73931b48ea9d149e19b77aac90a998c18e824d0
Dhanusree-max <dhanusriabburi53@gmail.com>
2026-02-26 16:39:02 +0530

    two

---
+++ day2/javascript/script.js
@@ -0,0 +1,20 @@
+let obj1={
+    name:'abc',
+    age:22
+}
+let obj2=obj1
+obj2.name='xyz'
+console.log(obj1.name)
+if(1  )
+{
+    console.log('true')
+}
+else{
+    console.log('false')
+}
+console.log(5+'5')
+console.log(5-'5')
+console.log(5=='5')
+
+let a=[1,'new',2,'3']
+console.log(a)

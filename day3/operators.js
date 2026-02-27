65c48f0802dbf1a66792893fc168f5680c77e932
Dhanusree-max <dhanusriabburi53@gmail.com>
2026-02-27 14:15:29 +0530

    hi

---
+++ day3/operators.js
@@ -0,0 +1,16 @@
+let a=20
+/*if(a%2==0)
+{
+    console.log("Even")
+}
+else{
+    console.log("Odd")
+}*/
+let r=a%2==0? 'Even' :'Odd'
+console.log(r)
+console.log(10&6)
+console.log(10|6)
+console.log(10^6)
+console.log(~10)
+console.log(10<<6)
+console.log(10>>6)
